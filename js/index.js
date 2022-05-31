b836940bf55c15415d9501ae0913d39dd6f7a06a
techie-adarsh <66101827+techie-adarsh@users.noreply.github.com>
2022-05-31 16:05:27 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://techie-adarsh.github.io";
+const baseURL = "https://inshare-xy.herokuapp.com/";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
